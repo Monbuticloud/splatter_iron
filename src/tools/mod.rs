e34c53e0c7fe29ba8c51ecb27394576faf9847ef
Monbuticloud <hoangquocanhmon@icloud.com>
2026-05-29 09:28:40 +0700

    docs: add inline doc for pub mod circle_brush

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -4,6 +4,7 @@
 pub mod brush_parsers;
 /// Scanline flood-fill tool implementation.
 pub mod bucket_fill;
+/// Midpoint-circle brush tool implementation.
 pub mod circle_brush;
 pub mod custom_brush;
 pub mod square_brush;
